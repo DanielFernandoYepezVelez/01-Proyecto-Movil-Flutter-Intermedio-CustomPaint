89124c8d57aa2a8bf6f927f9ec2ddfd6b53393b8
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-06-02 15:56:53 -0500

    Terminando La Secciones De Los Disenos

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -35,7 +35,12 @@ class HeaderDiagonal extends StatelessWidget {
       // color: Color(0xff615AAB),
       child: CustomPaint(
         // painter: _HeaderDiagonalPainter(),
-        painter: _HeaderTrianguloPainter(),
+        // painter: _HeaderTrianguloPainter(),
+        // painter: _HeaderPicoPainter(),
+        // painter: _HeaderCurvoPainter(),
+        // painter: _HeaderWavePainter(),
+        // painter: _HeaderWaveDownPainter(),
+        painter: _HeaderWaveGradientPainter(),
       ),
     );
   }
@@ -52,6 +57,7 @@ class _HeaderDiagonalPainter extends CustomPainter {
     pencil.strokeWidth = 2;
 
     final path = new Path();
+
     /* Movimientos Y Trazos Con El lapiz En El Canvas */
     path.moveTo(0, size.height * 0.35);
 
@@ -93,6 +99,186 @@ class _HeaderTrianguloPainter extends CustomPainter {
     // path.lineTo(size.width, size.height);
     // path.lineTo(0, size.height);
     // path.lineTo(0, 0);
+
+    /* Decirle Al Canvas Que Dibje */
+    canvas.drawPath(path, pencil);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pencil = new Paint();
+
+    /* Properties */
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; // .stroke
+    pencil.strokeWidth = 5;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.30);
+    path.lineTo(size.width * 0.50, size.height * 0.40);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+
+    /* Decirle Al Canvas Que Dibje */
+    canvas.drawPath(path, pencil);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pencil = new Paint();
+
+    /* Properties */
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; // .stroke
+    pencil.strokeWidth = 5;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.30);
+
+    /* Los Dos Primeros Valores => Por Donde Va A Pasar La Circunferencia */
+    /* Los Dos Ultimos Valores => Donde Termina La El Punto Final De la Linea */
+    path.quadraticBezierTo(
+        size.width * 0.50, size.height * 0.45, size.width, size.height * 0.30);
+    // path.quadraticBezierTo(x1, y1, x2, y2)
+
+    path.lineTo(size.width, 0);
+
+    /* Decirle Al Canvas Que Dibje */
+    canvas.drawPath(path, pencil);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pencil = new Paint();
+
+    /* Properties */
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; // .stroke
+    pencil.strokeWidth = 5;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.30);
+
+    /* Los Dos Primeros Valores => Por Donde Va A Pasar La Circunferencia */
+    /* Los Dos Ultimos Valores => Donde Termina La El Punto Final De la Linea */
+    path.quadraticBezierTo(size.width * 0.20, size.height * 0.40,
+        size.width * 0.50, size.height * 0.30);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.20, size.width, size.height * 0.30);
+    // path.quadraticBezierTo(x1, y1, x2, y2)
+
+    path.lineTo(size.width, 0);
+
+    /* Decirle Al Canvas Que Dibje */
+    canvas.drawPath(path, pencil);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _HeaderWaveDownPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pencil = new Paint();
+
+    /* Properties */
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; // .stroke
+    pencil.strokeWidth = 25;
+
+    final path = new Path();
+
+    /* Movimientos Y Trazos Con El lapiz En El Canvas */
+    path.moveTo(0, size.height);
+    path.lineTo(0, size.height * 0.70);
+
+    /* Los Dos Primeros Valores => Por Donde Va A Pasar La Circunferencia */
+    /* Los Dos Ultimos Valores => Donde Termina La El Punto Final De la Linea */
+    path.quadraticBezierTo(size.width * 0.20, size.height * 0.60,
+        size.width * 0.50, size.height * 0.70);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.80, size.width, size.height * 0.70);
+    // path.quadraticBezierTo(x1, y1, x2, y2)
+
+    path.lineTo(size.width, size.height);
+
+    /* Decirle Al Canvas Que Dibje */
+    canvas.drawPath(path, pencil);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _HeaderWaveGradientPainter extends CustomPainter {
+  /* TRABAJAR CON GRADIENTES */
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = new Rect.fromCircle(
+      center: Offset(0, 55),
+      radius: 180,
+    );
+
+    final Gradient gradient = new LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xff6D05E8),
+        Color(0xffC012FF),
+        Color(0xff6D05FA),
+      ],
+      stops: [0.2, 0.5, 1],
+    );
+
+    final pencil = new Paint()..shader = gradient.createShader(rect);
+
+    /* Properties */
+    pencil.color = Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill; // .stroke
+    pencil.strokeWidth = 5;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.30);
+
+    /* Los Dos Primeros Valores => Por Donde Va A Pasar La Circunferencia */
+    /* Los Dos Ultimos Valores => Donde Termina La El Punto Final De la Linea */
+    path.quadraticBezierTo(size.width * 0.20, size.height * 0.40,
+        size.width * 0.50, size.height * 0.30);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.20, size.width, size.height * 0.30);
+    // path.quadraticBezierTo(x1, y1, x2, y2)
+
+    path.lineTo(size.width, 0);
 
     /* Decirle Al Canvas Que Dibje */
     canvas.drawPath(path, pencil);
